20f3d319371dc75ac898086be19faac9dffce63c
ZIHAD22 <mdzihad2244@gmail.com>
2022-01-04 17:15:59 +0600

    all function is work fine

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import "./style/timer.style.css";
+
+class App extends Component {
+  state = {
+    count: 0,
+  };
+  intervalId = null;
+
+  incrementCount = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  decrementCount = () => {
+    if (this.state.count > 0) {
+      this.setState({ count: this.state.count - 1 });
+    }
+  };
+
+  startTimer = () => {
+    this.intervalId = setInterval(() => {
+      this.setState({ count: this.state.count - 1 }, () => {
+        if (this.state.count === 0) {
+          alert("Time is Finished");
+          clearInterval(this.intervalId);
+          this.intervalId = null;
+        }
+      });
+    }, 1000);
+  };
+
+  stopTimer = () => {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
+  };
+
+  resetTimer = () => {
+    if (this.intervalId) {
+      this.setState({ count: 0 });
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>This is Timer</h1>
+        <div className="container">
+          <button onClick={this.decrementCount} className="btn">
+            -
+          </button>
+          <span>{this.state.count}</span>
+          <button onClick={this.incrementCount} className="btn">
+            +
+          </button>
+          <div className="main-func">
+            <button onClick={this.startTimer} className="btn Start">
+              Start
+            </button>
+            <button onClick={this.stopTimer} className="btn Stop">
+              Stop
+            </button>
+            <button onClick={this.resetTimer} className="btn Reset">
+              Reset
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
